e8e338d854a6de061b807088957a7fe5a83a26fc
Daniel Brady <daniel.13rady@gmail.com>
2023-09-09 13:48:32 +0100

    feat: Implement real 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,49 +1,39 @@
-import * as React from "react"
-import { Link } from "gatsby"
+/** @jsxImportSource theme-ui */
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import React from "react";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage = () => {
+export default function NotFoundPage() {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main sx={{
+      margin: ['1.5rem', '0 5.5rem'],
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <section sx={{
+        minHeight: '100vh',
+        paddingTop: ['2rem', '4rem'],
+      }}>
+        <pre>
+          {`
+     __//
+    /.__.\\
+    \\ \\/ /
+ '__/    \\
+  \\-      )
+   \\_____/
+_____|_|____
+     " "
+`}
+        </pre>
+        <p>
+          This place doesn't exist.... The sadness.
+        </p>
+        <p>
+          ...But then of course, if this place doesn't exist, are you really here?
+        </p>
+      </section>
     </main>
-  )
+  );
 }
 
-export default NotFoundPage
-
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Nothing to see here...</title>;
